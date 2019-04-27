1bcb6751e5dafd1df61265d4df7e09206e509e3a
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-04-27 23:55:45 +0200

    elixir version restriction

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Solution.MixProject do
     [
       app: :solution,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
