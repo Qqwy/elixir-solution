c5b18081e31e116f71d7e79ce096dd6048d560cf
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-04-28 21:49:24 +0200

    bumps Mix version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Solution.MixProject do
   def project do
     [
       app: :solution,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
